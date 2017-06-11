c7ad99e777531ed44b9066cc0c5a4891d7e55d21
Isaac Cook <isaac@simpload.com>
2017-06-10 20:02:49 -0500

    Fix typo

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='simplecoin_rpc_client',
       entry_points={
           'console_scripts': [
               'simplecoin_rpc_scheduler = simplecoin_rpc_client.scheduler:entry',
-              'simplecoin_rpc = simplecoin_rpc_client.rpc:entry'
+              'simplecoin_rpc = simplecoin_rpc_client.sc_rpc:entry'
           ]
       },
       packages=find_packages()
